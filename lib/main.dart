f957ac09652f091dd9782e9fac7798d65b9373f8
SamsonMokaya <mokayasamson@gmail.com>
2023-02-27 17:32:48 +0300

    Image labelling

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
 import 'package:image_picker/image_picker.dart';
 
 late List<CameraDescription> _cameras;
@@ -9,11 +10,11 @@ late List<CameraDescription> _cameras;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   _cameras = await availableCameras();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -24,6 +25,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -46,52 +48,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   late CameraController cameraController;
   File? _image;
-  final ImagePicker _picker = ImagePicker();
-
-
-  chooseImages() async{
-
-    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-
-    _image = File(image!.path);
-
-    if(image != null){
-
-      setState(() {
-        _image = File(image.path);
-        doImageLabelling();
-      });
-
-    }
-
-  }
-
-  captureImages() async{
-
-    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
-
-    _image = File(image!.path);
-
-    if(image != null){
-
-      setState(() {
-        _image = File(image.path);
-        doImageLabelling();
-      });
-
-    }
-
-  }
-
-  doImageLabelling() {
-
-  }
-
+  late ImagePicker imagePicker;
+  String result = "Results will be shown here";
 
 
   @override
   void initState() {
     super.initState();
+
+    imagePicker = ImagePicker();
+
     cameraController = CameraController(_cameras[0], ResolutionPreset.max);
     cameraController.initialize().then((_) {
       if (!mounted) {
@@ -112,53 +78,141 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  @override
+  void dispose(){
+    super.dispose();
+  }
+
+
+  chooseImagesFromGallery() async{
+
+    XFile? image = await imagePicker.pickImage(source: ImageSource.gallery);
+
+    _image = File(image!.path);
+
+    if(image != null){
+
+      setState(() {
+        _image = File(image.path);
+        doImageLabelling();
+      });
+
+    }
+
+  }
+
+  captureImagesIWithCamera() async{
+
+    XFile? image = await imagePicker.pickImage(source: ImageSource.camera);
+
+    _image = File(image!.path);
+
+    if(image != null){
+
+      setState(() {
+        _image = File(image.path);
+        doImageLabelling();
+      });
+
+    }
+
+  }
+
+  doImageLabelling() async{
+
+    InputImage inputImage = InputImage.fromFile(_image!!);
+
+  }
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
 
-          children: <Widget>[
+    return Container(
+      child: Scaffold(
+        body: SingleChildScrollView(
+            child: Column(
 
-        Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(16.0),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 8.0,
-              spreadRadius: 4.0,
-              offset: Offset(0, 4),
+
+              children: <Widget>[
+                SizedBox(
+                  width: 100,
+                ),
+
+            Container(
+              margin: EdgeInsets.only(top: 100),
+              child: Stack(
+                children: [
+                  Image.asset("assets/frame.png",
+                  height: 510,
+                    width: 500,
+
+                  ),
+                  Center(
+                    child:ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.transparent,
+                        shadowColor: Colors.transparent,
+
+                      ),
+                        onPressed: (){
+
+                      chooseImagesFromGallery();
+
+                    },onLongPress: (){
+
+                      captureImagesIWithCamera();
+
+                    },
+                      child: Container(
+                        margin: EdgeInsets.only(top: 8),
+                        child: _image != null?Image.file(
+                            _image!,
+                          width: 335,
+                          height: 495,
+                          fit: BoxFit.fill,
+                        )
+                            :Container(
+                          width: 340,
+                          height: 330,
+                          child: Icon(
+                            Icons.camera_alt,
+                            color: Colors.black
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ],
-        ),
-        width: 400,
-        height: 450 ,
 
-          child: _image != null?Image.file(_image!):Icon(Icons.image, size: 150,),
-        ),
-
-
-
-        ElevatedButton(onPressed: (){
-
-              chooseImages();
-
-            },onLongPress: (){
-
-              captureImages();
-
-            }, child: Text("Choose / Capture")),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                  ),
+                  child: Text(
+                    result,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
 
 
-          ],
+
+
+
+
+
+
+              ],
+            ),
         ),
       ),
     );
